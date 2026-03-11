f98d625232b453b4c3649ead971fd82961aea62b
djzhhil <zguangqi52@gmail.com>
2026-03-11 16:06:54 +0800

    前后端代码放一块保存

--- back/src/main/java/com/example/idphoto/IdPhotoApplication.java
+++ back/src/main/java/com/example/idphoto/IdPhotoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IdPhotoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(IdPhotoApplication.class, args);
     }
